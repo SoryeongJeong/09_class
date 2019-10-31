90a6175920f33a6f33b9fdfc474db8be24f9f21d
SoryeongJeong <55128033+SoryeongJeong@users.noreply.github.com>
2019-10-31 15:32:52 +0900

    op_3(2)

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int grade[5];
+	int a[5] = {1,2,3,4,5};
+	int b[5] = {1,2,3,4,5};
 	
-	grade[0] = 10;
-	grade[1] = 20;
-	grade[2] = 30;
-	grade[3] = 40;
-	grade[4] = 50;
-	
-	for (i=0;i<5;i++)
+	for(i=0; i<5;i++)
+	{
+		if(a[i] != b[i])
 		{
-			printf("grade[%d] = %d\n", i, grade[i]);
+			printf("%i is differnet\n", i);
 		}
-	return 0;
+	}
+
+return 0;
+
 }
